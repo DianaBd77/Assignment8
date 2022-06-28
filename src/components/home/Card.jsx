71c9f6a802d1aefe3df87f7a5155c3808464482c
devops <diana.behshad@gmail.com>
2022-06-28 11:19:05 +0430

    Create Card component

---
+++ src/components/home/Card.jsx
@@ -0,0 +1,26 @@
+import "./Home.css";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+const ImgCard = ({ title, description, img }) => {
+  return (
+    <Card className="card" sx={{ maxWidth: 250 }}>
+      <CardMedia  className="card-img" component="img" alt="green iguana" height="100" image={img} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions></CardActions>
+    </Card>
+  );
+};
+
+export default ImgCard;
