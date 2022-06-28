41038980d0fdd9779c2d9558d8fa61f162a5d964
devops <diana.behshad@gmail.com>
2022-06-28 11:18:16 +0430

    Create Footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "../../App.css";
+import "./Footer.css";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="footer">
+        <p>Summer and Flower</p>
+    </div>
+  );
+};
+
+export default Home;
